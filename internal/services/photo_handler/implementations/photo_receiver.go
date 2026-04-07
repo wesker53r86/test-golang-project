62d29e703a2f10ce0bc8698887b4ef275c9717dc
Rey Padua <rehumpadua@gmail.com>
2026-04-08 06:16:02 +0800

    completed test

--- internal/services/photo_handler/implementations/photo_receiver.go
+++ internal/services/photo_handler/implementations/photo_receiver.go
@@ -1,14 +1,27 @@
 package photo_receiver
 
 import (
+	"net/http"
 	"test-golang-project/internal/models"
 )
 
 func ReceivePhoto(photo_name string, data []byte) models.PhotoObject{
+	mime := http.DetectContentType(data)
+
 	ret := models.PhotoObject{
 		PictureName: photo_name,
-		MimeType: "image/png",
-		Bytes: []byte("hi there"),
+		MimeType: mime,
+		Bytes: data,
+	}
+	return ret
+}
+
+func ReceivePhotoFromBytes(name string, bytes []byte) models.PhotoObject{
+	mime := http.DetectContentType(bytes)
+	ret := models.PhotoObject{
+		PictureName: name,
+		MimeType: mime,
+		Bytes: bytes,
 	}
 	return ret
 }
--- internal/services/photo_handler/tests/photo_receiver_test.go
+++ internal/services/photo_handler/tests/photo_receiver_test.go
@@ -2,19 +2,41 @@ package photo_receiver
 
 import (
 	"fmt"
-	photo_receiver "test-golang-project/internal/services/photo_handler/implementations"
+	"os"
 	"test-golang-project/internal/models"
+	photo_receiver "test-golang-project/internal/services/photo_handler/implementations"
 	"testing"
 ) 
 
 func TestSimple(t *testing.T){
-	fmt.Println("hello there")
 	var vit models.PhotoObject = photo_receiver.ReceivePhoto("mignle",[]byte("hello world"))
 	if vit.PictureName != "mignle"{
-		t.Errorf("man there's an error here")
+		t.Errorf("The name of the Photo Object is incorrect")
 	}
 }
 
 func TestProcessImage(t *testing.T){
-	
+	file_path := "./sample_files/sample_picture.png"
+	//load the picture
+	content, err := os.ReadFile(file_path)
+	if err != nil{
+		fmt.Println("There was an issue reading the file:",err)
+		return
+	}
+	//call the method
+	comp := photo_receiver.ReceivePhotoFromBytes("sample_picture",content)
+
+	//check the values
+	if comp.PictureName != "sample_picture"{
+		t.Errorf("The name of the Photo Object is incorrect")
+	}
+
+	if comp.MimeType != "image/png"{
+		t.Errorf("the mimetype of the image is incorrect")
+	}
+
+	if len(comp.Bytes) == 0{
+		t.Errorf("There is no data in the photo object")
+	}
+
 }
